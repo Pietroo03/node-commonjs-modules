95718465de8e1794279b7ab076a6aef9c3cdbc7d
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-29 15:51:54 +0100

    create file hobbies.js
    add a function to hobbies.js
    
    add require method to main.js

---
+++ hobbies.js
@@ -0,0 +1,10 @@
+/* Crea una funzione che accetta tre parametri: hobbyOne, hobbyTwo, hobbyThree. La funzione dovrebbe restituire un oggetto con una proprietà hobbies, che è un array di hobby.
+Esporta la funzione dal file. */
+
+function hobbiesList(hobbyOne, hobbyTwo, hobbyThree) {
+    return element = {
+        "hobbies": [hobbyOne, hobbyTwo, hobbyThree]
+    }
+}
+
+module.exports = hobbiesList
--- main.js
+++ main.js
@@ -3,3 +3,5 @@ console.log('ciao');
 const fullName = require("./names.js")
 console.log(fullName('Pietro', 'Ponte'));
 
+const hobbiesList = require("./hobbies.js")
+console.log(hobbiesList('Calcio', 'Nuoto', 'Leggere'));
