434f4d6b1b9269c09b4bc9c0bfe733ac3a32da6f
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-29 16:04:33 +0100

    add function to main.js

--- main.js
+++ main.js
@@ -5,3 +5,16 @@ console.log(fullName('Pietro', 'Ponte'));
 
 const hobbiesList = require("./hobbies.js")
 console.log(hobbiesList('Calcio', 'Nuoto', 'Leggere'));
+
+/* Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con tre proprietà: firstName, lastName, hobbies. */
+
+function people() {
+    return person = {
+        "firstName": fullName('Pietro', 'Ponte').firstName,
+        "lastName": fullName('Pietro', 'Ponte').lastName,
+        "hobbies": hobbiesList('Calcio', 'Nuoto', 'Leggere').hobbies
+    }
+        
+}
+
+console.log(people());
